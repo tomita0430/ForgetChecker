99a6dc8ff40eef1316b68da1781a4269475160f1
tsuttsus <brvotsuttsu@gmail.com>
2023-10-07 12:06:56 +0900

    fix

--- ForgetCheckerApp/src/App.js
+++ ForgetCheckerApp/src/App.js
@@ -1,10 +1,74 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState  } from 'react';
+import { BleManager } from 'react-native-ble-plx';
 import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-native';
 
+const App = () => {
+  console.log("Start Application");
 
+  const [memo, setMemo] = useState('');
+  const [memoList, setMemoList] = useState([]);
+
+  const [devices, setDevices] = useState([]);
+
+  /*
+  // Bluetooth対応端末であれば，こちらを実行
+  const manager = new BleManager()
+  useEffect(() => {
+    const subscription = bleManager.onStateChange((state) => {
+      if (state === 'PoweredOn') {
+        startScanning();
+      }
+    }, true);
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  const startScanning = () => {
+    bleManager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        console.error('Error scanning for BLE devices:', error);
+        return;
+      }
+
+      // Add the scanned device to the devices array
+      setDevices((prevDevices) => [...prevDevices, device]);
+    });
+  };
+
+
+  return (
+    <div>
+      <h1>BLE Device Scanner</h1>
+      <ul>
+        {devices.map((device, index) => (
+          <li key={index}>{device.name || 'Unknown Device'}</li>
+        ))}
+      </ul>
+    </div>
+  );
+  */
+  
+  return (
+  <View>
+    <Text>BLE</Text>
+    <Text>{devices.length}</Text>
+    <FlatList
+        data={devices}
+        renderItem={({ item }) => <Text>{item}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+    />
+  </View>
+  )
+};
+
+export default App;
+/*
 const App = () => {
     const [memo, setMemo] = useState('');
     const [memoList, setMemoList] = useState([]);
+    console.log("Hello");
 
     const addMemo = () => {
         if (memo) {
@@ -50,4 +114,5 @@ const styles = StyleSheet.create({
     },
 });
 
-export default App; 
+export default App;
+*/
